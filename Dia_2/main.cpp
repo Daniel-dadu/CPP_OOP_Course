96d75e188071fa59eff8b7e304b57d102dae7d83
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-05-04 18:26:28 -0500

    Improved main and uploaded video link from Dia_2

--- Dia_2/main.cpp
+++ Dia_2/main.cpp
@@ -13,37 +13,39 @@ int main(){
 
   Persona personaUno("Antonio", "Hernández", "Torres", 23, 'M', 1.75, "Negro"); // Objeto (instancia de una clase)
   personaUno.imprimirPersona();
-  // cout << endl;
-  // Persona personaDos("María", "Herrera", "Martínez", 'F'); // Objeto (instancia de una clase)
-  // personaDos.imprimirPersona();
   
-  // Persona personaTres("Mario", "Torres", "Lozano", 'M');
-  // personaTres.imprimirPersona();
+  cout << endl;
+  Persona personaDos("María", "Herrera", "Martínez", 'F'); // Objeto (instancia de una clase)
+  personaDos.imprimirPersona();
   
-//   cout << endl;
-  // Persona personaTres("Lalo"); // Objeto (instancia de una clase)
-  // personaTres.imprimirPersona(); // A pesar de no haber definido los demás atributos, no marca error
+  cout << endl;
+  Persona personaTres("Mario", "Torres", "Lozano", 'M');
+  personaTres.imprimirPersona();
+  
+  cout << endl;
+  Persona personaCuatro("Lalo"); // Objeto (instancia de una clase)
+  personaCuatro.imprimirPersona(); // A pesar de no haber definido los demás atributos, no marca error
 
-//   cout << endl;
-  // cout << personaDos.getNombre() << " tiene " << personaDos.getEdad() 
-  //       << " años y mide " << personaDos.getEstatura() << " metros." << endl;
+  cout << endl;
+  cout << personaDos.getNombre() << " tiene " << personaDos.getEdad() 
+        << " años y mide " << personaDos.getEstatura() << " metros." << endl;
   
-  // personaDos.setEdad(27); // A través de los setters vamos a cambiar los atributos de un objeto
-  // personaDos.setEstatura(1.78);
-  // cout << "Ahora " << personaDos.getNombre() << " tiene " << personaDos.getEdad() 
-  //       << " años y mide " << personaDos.getEstatura() << " metros." << endl;
+  personaDos.setEdad(27); // A través de los setters vamos a cambiar los atributos de un objeto
+  personaDos.setEstatura(1.78);
+  cout << "Ahora " << personaDos.getNombre() << " tiene " << personaDos.getEdad() 
+        << " años y mide " << personaDos.getEstatura() << " metros." << endl;
 
-  // cout << endl;
-  // personaUno.estadoHambreSed(); // identificadorSed = false;
-  // personaUno.correr(); // identificadorSed = true;
-  // personaUno.estadoHambreSed(); // identificadorSed = true;
-  // personaUno.beber(); // identificadorSed = false;
-  // personaUno.estadoHambreSed(); // identificadorSed = false;
-  // cout << endl;
-  // personaUno.despertar();
-  // personaUno.estadoHambreSed(); // identificadorHambre = true;
-  // personaUno.comer(); // identificadorHambre = false;
-  // personaUno.estadoHambreSed(); // identificadorHambre = false;
+  cout << endl;
+  personaUno.estadoHambreSed(); // identificadorSed = false;
+  personaUno.correr(); // identificadorSed = true;
+  personaUno.estadoHambreSed(); // identificadorSed = true;
+  personaUno.beber(); // identificadorSed = false;
+  personaUno.estadoHambreSed(); // identificadorSed = false;
+  cout << endl;
+  personaUno.despertar();
+  personaUno.estadoHambreSed(); // identificadorHambre = true;
+  personaUno.comer(); // identificadorHambre = false;
+  personaUno.estadoHambreSed(); // identificadorHambre = false;
   
   return 0; // Fin del programa
 
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Impartido por *Daniel Maldonado* y *LozanoSoft*
 ## Links de grabaciones de las clases:
 
 - Día 1 (03/05/2021): https://youtu.be/MvyEnkPDXGk
-- Día 2 (04/05/2021):
+- Día 2 (04/05/2021): https://youtu.be/IRwUWSKeSZo
 - Día 3 (05/05/2021):
 - Día 4 (06/05/2021):
 - Día 5 (07/05/2021):
